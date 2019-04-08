87854132bf559c56ace680c2af00ace9e90a785b
Neon Dolphin <48138182+NeonDolphin@users.noreply.github.com>
2019-04-08 05:15:21 +0200

    Add files via upload

---
+++ while_loop2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+int main()
+{
+	int pin = 0;
+	int tries = 0;
+	
+	std::cout << "Welcome to the RusBank!\n";
+	
+	
+	std::cout << "Enter your PIN:\n";
+	std::cin >> pin;
+	
+	while(pin != 1234 && tries <= 3){
+		
+	std::cout << "Enter your PIN:\n";
+	std::cin >> pin;
+	tries++;
+}
+     
+	if(pin == 1234){
+		
+	std::cout << "Pin accepted\n";
+	std::cout << "You can now take the damn money!\n";
+}
+    if(pin != 1234){
+		
+    std::cout << "We will call the cops!\n";
+}
+return 0;
+	
+	}
