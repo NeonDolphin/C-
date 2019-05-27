845b6293f8dfba4e48da2923b1779566f88fbada
Neon Dolphin <48138182+NeonDolphin@users.noreply.github.com>
2019-05-27 03:49:02 +0200

    Create calc_for_loop.cpp
    calculation in for loop

---
+++ calc_for_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ int main(){
+
+
+     int bubba = 44;
+     int year = 10;
+
+
+    for(int x = 0; x < 10; x++){
+    bubba += year;
+    std::cout << bubba << std::endl; 
+
+}
+return 0;
+}
